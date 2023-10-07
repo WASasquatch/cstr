d3c12269048bc8177af357aa1a9e35d1c253d708
WAS <jordanslost@gmail.com>
2023-10-06 21:21:35 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cstr',
+    version='0.0.1',
+    url='https://github.com/WASasquatch/cstr.git',
+    author='WAS',
+    author_email='jordanslost@gmail.com',
+    description='Provides a method for printing colored messages to console with preset tags.',
+    packages=find_packages(),
+    install_requires=[],
+)
